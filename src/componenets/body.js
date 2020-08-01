256e303af4993ecfaf10d1370246227f1506acf3
rilwan <olawuyirilwan98@gmail.com>
2020-08-01 09:04:36 +0100

    my first page

---
+++ src/componenets/body.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+    const Body =  (props) => {  
+        return (
+            <div>
+                <p>This is our month of  celebration</p>
+            </div>
+        )
+    }
+
+export default Body;
---
+++ src/componenets/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+    const Home =  (props) => {  
+        return (
+            <div>
+                <p>Good morning  everyone</p>
+            </div>
+        )
+    }
+
+export default Home;
