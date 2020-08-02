b1d06c99901225fb1b9b050936b5d0798173bd9b
rilwan <olawuyirilwan98@gmail.com>
2020-08-02 11:46:22 +0100

    react hooks and context api

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,10 +3,10 @@ import AppReducer from './AppReducer';
 
 const initialState ={
     transactions :[
-        { id: 1, text: 'Flower', amount: -20 },
-       { id: 2, text: 'Salary', amount: 300 },
-       { id: 3, text: 'Book', amount: -10 },
-       { id: 4, text: 'Camera', amount: 150 }
+    //     { id: 1, text: 'Flower', amount: -20 },
+    //    { id: 2, text: 'Salary', amount: 300 },
+    //    { id: 3, text: 'Book', amount: -10 },
+    //    { id: 4, text: 'Camera', amount: 150 }
     ]
 }
 
@@ -17,7 +17,23 @@ export const GlobalContext = createContext(initialState);
 export const  GlobalProvider = ({ children }) =>{
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (    <GlobalContext.Provider value={{transactions: state.transactions}}>
+    //actions
+    function deleteTransaction(id){
+        dispatch ({ 
+            type:'DELETE_TRANSACTION',
+            payload:id
+         });
+    }
+
+     function addTransaction(transaction){
+        dispatch ({ 
+            type:'ADD_TRANSACTION',
+            payload: transaction
+         });
+    }
+
+    return (    <GlobalContext.Provider value={{transactions: state.transactions,
+    deleteTransaction, addTransaction}}>
         {children}
         </GlobalContext.Provider>)
 }
