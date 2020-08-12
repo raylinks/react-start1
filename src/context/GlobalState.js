d4ab66675804a007fa4162cedd4a510e8cc40ad7
rilwan <olawuyirilwan98@gmail.com>
2020-08-12 08:22:12 +0100

    global state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,7 +14,7 @@ const initialState ={
 export const GlobalContext = createContext(initialState);
 
 //provider component
-export const  GlobalProvider = ({ children }) =>{
+export const  GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     //actions
